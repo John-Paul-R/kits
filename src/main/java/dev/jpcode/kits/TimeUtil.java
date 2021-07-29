cd85f4951dd6cba7033864ea2deadab4238e4c27
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2021-07-29 18:47:37 -0400

    Conform to checkstyle. (oops)

--- src/main/java/dev/jpcode/kits/TimeUtil.java
+++ src/main/java/dev/jpcode/kits/TimeUtil.java
@@ -1,33 +1,29 @@
 package dev.jpcode.kits;
 
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.Suggestions;
-
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-
-import net.minecraft.server.command.ServerCommandSource;
-
-import java.text.ParseException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+
+import net.minecraft.server.command.ServerCommandSource;
+
 public final class TimeUtil {
 
     private TimeUtil() {}
 
-    private static final LinkedHashMap<String, Long> timeWeights = new LinkedHashMap<>();
+    private static final LinkedHashMap<String, Long> TIME_WEIGHTS = new LinkedHashMap<>();
 
     static {
-        timeWeights.put("y",  1000L * 60 * 60 * 24 * 365);
-        timeWeights.put("d",  1000L * 60 * 60 * 24);
-        timeWeights.put("h",  1000L * 60 * 60 );
-        timeWeights.put("m",  1000L * 60 );
-        timeWeights.put("s",  1000L);
+        TIME_WEIGHTS.put("y", 1000L * 60 * 60 * 24 * 365);
+        TIME_WEIGHTS.put("d", 1000L * 60 * 60 * 24);
+        TIME_WEIGHTS.put("h", 1000L * 60 * 60);
+        TIME_WEIGHTS.put("m", 1000L * 60);
+        TIME_WEIGHTS.put("s", 1000L);
 
     }
 
@@ -41,18 +37,19 @@ public final class TimeUtil {
 
         return parseToMillis(num, timeId);
     }
+
     public static long parseToMillis(long time, String timeUnit) {
-        return time * timeWeights.get(timeUnit);
+        return time * TIME_WEIGHTS.get(timeUnit);
     }
 
     public static CompletableFuture<Suggestions> suggestions(CommandContext<ServerCommandSource> context, SuggestionsBuilder builder) {
-        return ListSuggestion.getSuggestionsBuilder(builder, timeWeights.keySet());
+        return ListSuggestion.getSuggestionsBuilder(builder, TIME_WEIGHTS.keySet());
     }
 
     public static String formatTime(long millis) {
         int i = 0;
         StringBuilder builder = new StringBuilder();
-        for (Map.Entry<String, Long> entry : timeWeights.entrySet()) {
+        for (Map.Entry<String, Long> entry : TIME_WEIGHTS.entrySet()) {
             String unitStr = entry.getKey();
             Long unitMsValue = entry.getValue();
 
@@ -60,7 +57,7 @@ public final class TimeUtil {
             i++;
             if (val != 0) {
                 builder.append(val).append(unitStr);
-                if (i != timeWeights.size()) {
+                if (i != TIME_WEIGHTS.size()) {
                     builder.append(" ");
                 }
             }
