aa6326362fdc1e258e92a36ca4a48ad5a0d3784f
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-01-21 21:31:54 -0500

    fix imports order

--- src/main/java/dev/jpcode/kits/Kit.java
+++ src/main/java/dev/jpcode/kits/Kit.java
@@ -9,7 +9,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
+
 
 public class Kit {
 
