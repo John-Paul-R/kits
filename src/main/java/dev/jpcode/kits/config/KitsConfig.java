0c179541694e10f81dc714b6228eb2077aaca7cc
ArnoKeesmand <arnokeesman@gmail.com>
2023-09-22 16:39:26 +0200

    starterkit fixes receiving your starterkit will also execture kit commands now config option added that sets kit cooldown for starterkits as well (enabled by default)

--- src/main/java/dev/jpcode/kits/config/KitsConfig.java
+++ src/main/java/dev/jpcode/kits/config/KitsConfig.java
@@ -8,6 +8,8 @@ public final class KitsConfig extends Config {
 
     @ConfigOption
     public final Option<String> starterKit = new Option<>("starter_kit", "", String::valueOf);
+    @ConfigOption
+    public final Option<Boolean> starterKitSetCooldown = new Option<>("starter_kit_set_cooldown", true, Boolean::getBoolean);
 
     public KitsConfig(Path savePath, String displayName, String documentationLink) {
         super(savePath, displayName, documentationLink);
