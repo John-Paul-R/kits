77a70a472920f0dc64d2a4fac150d50425379ef3
ArnoKeesmand <arnokeesman@gmail.com>
2024-05-30 17:10:07 +0200

    update to 1.21-pre1

--- src/main/java/dev/jpcode/kits/mixin/PlayerManagerMixin.java
+++ src/main/java/dev/jpcode/kits/mixin/PlayerManagerMixin.java
@@ -1,7 +1,5 @@
 package dev.jpcode.kits.mixin;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,12 +7,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ConnectedClientData;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.TeleportTarget;
 
 import dev.jpcode.kits.PlayerDataManager;
 import dev.jpcode.kits.events.PlayerConnectCallback;
@@ -50,35 +49,30 @@ public abstract class PlayerManagerMixin {
             target = "Lnet/minecraft/server/network/ServerPlayerEntity;copyFrom(Lnet/minecraft/server/network/ServerPlayerEntity;Z)V"
         ),
         locals = LocalCapture.CAPTURE_FAILHARD)
-    public void onRespawnPlayer(
-        ServerPlayerEntity oldServerPlayerEntity
-        , boolean alive
-        , CallbackInfoReturnable<ServerPlayerEntity> cir
-        , BlockPos blockPos
-        , float f
-        , boolean bl
-        , ServerWorld serverWorld
-        , Optional optional
-        , ServerWorld serverWorld2
-        , ServerPlayerEntity serverPlayerEntity
-    ) {
+    public void onRespawnPlayer(ServerPlayerEntity oldServerPlayerEntity,
+                                boolean alive,
+                                Entity.RemovalReason removalReason,
+                                CallbackInfoReturnable<ServerPlayerEntity> cir,
+                                TeleportTarget teleportTarget,
+                                ServerWorld serverWorld,
+                                ServerPlayerEntity serverPlayerEntity) {
         PlayerDataManager.handlePlayerDataRespawnSync(oldServerPlayerEntity, serverPlayerEntity);
     }
 
+    @SuppressWarnings("InvalidInjectorMethodSignature")
     @Inject(method = "respawnPlayer", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/server/world/ServerWorld;getLevelProperties()Lnet/minecraft/world/WorldProperties;"
         ), locals = LocalCapture.CAPTURE_FAILHARD)
-    public void onRespawnPlayer(ServerPlayerEntity oldServerPlayerEntity, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir,
-                                BlockPos blockPos,
-                                float f,
-                                boolean bl,
+    public void onRespawnPlayer(ServerPlayerEntity oldServerPlayerEntity,
+                                boolean alive,
+                                Entity.RemovalReason removalReason,
+                                CallbackInfoReturnable<ServerPlayerEntity> cir,
+                                TeleportTarget teleportTarget,
                                 ServerWorld serverWorld,
-                                Optional optional2,
-                                ServerWorld serverWorld2,
                                 ServerPlayerEntity serverPlayerEntity,
-                                boolean bl2
-    ) {
+                                byte b,
+                                ServerWorld serverWorld2) {
         PlayerRespawnCallback.EVENT.invoker().onPlayerRespawn(oldServerPlayerEntity, serverPlayerEntity);
     }
 }
