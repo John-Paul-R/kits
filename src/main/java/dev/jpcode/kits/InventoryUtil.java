81dca7cae40d6dba579b45f6aeee376286dca0ff
ArnoKeesmand <arnokeesman@gmail.com>
2026-09-12 14:21:16 +0200

    update to 26.3-rc2

--- src/main/java/dev/jpcode/kits/InventoryUtil.java
+++ src/main/java/dev/jpcode/kits/InventoryUtil.java
@@ -1,5 +1,6 @@
 package dev.jpcode.kits;
 
+import net.minecraft.util.Prediction;
 import net.minecraft.world.entity.player.Inventory;
 
 public final class InventoryUtil {
@@ -8,7 +9,7 @@ public final class InventoryUtil {
 
     public static void offerAllCopies(KitInventory source, Inventory target) {
         for (int i = 0; i < source.getContainerSize(); ++i) {
-            target.placeItemBackInInventory(source.getItem(i).copy());
+            target.placeItemBackInInventory(source.getItem(i).copy(), Prediction.SERVER_ONLY);
         }
     }
 }
